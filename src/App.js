f7853610afbeb04f07b872db2ac8b81e8dd4ff10
jonisland <jogucareers@gmail.com>
2022-06-28 08:25:48 -0700

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.scss";
 import HomePage from "./components/home-page";
 import { Switch, Route } from "react-router-dom";
+import NotFound from "./components/not-found";
+import Shop from "./components/pages/shop/shop";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={Shop} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </div>
   );
---
+++ src/components/pages/shop/shop.jsx
@@ -0,0 +1,22 @@
+import "./shop.styles.scss";
+import React, { useContext } from "react";
+import Layout from "../../shared/layout";
+import FeaturedProduct from "../../shared/featured-product";
+import { ProductsContext } from "../../../context/products-context";
+
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
+  const allProducts = products.map((product) => (
+    <FeaturedProduct {...product} key={product.id} />
+  ));
+  return (
+    <Layout>
+      <div className="product-list-container">
+        <h2 className="product-list-title">Shop</h2>
+        <div className="product-list">{allProducts}</div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Shop;
